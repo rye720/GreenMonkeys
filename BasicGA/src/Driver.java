44270c2b1bb7db0d6573254fa3c00cdcdde169c7
James Warth <jaw093@yahoo.com>
2015-11-09 19:27:30 -0500

    minor changes

--- BasicGA/src/Driver.java
+++ BasicGA/src/Driver.java
@@ -11,17 +11,25 @@ public class Driver {
         GAUtils gUtils = new GAUtils();
         Visuals v = new Visuals();
         Scanner s = new Scanner(System.in);
+        int generations;
+        int animalNum;
+        int geneNum;
         //Generate popualtion, get their fitness, and rank them
-        List<Animal> population = ga.GeneratePop("Ursus americanus", 10, 4, 0);
+        System.out.println("How many genes? ");
+        geneNum = s.nextInt();
+        System.out.println("How many animals? ");
+        animalNum = s.nextInt();
+        List<Animal> population = ga.GeneratePop("Ursus americanus", animalNum, geneNum, 0);
         ga.EvalPopFitness(population);
         population = gUtils.ranking(population);
-        int generations = 20;
 
         System.out.println("Visual mode? y or n: ");
-        String str = s.nextLine();
+        String str = s.next();
         if (str.equalsIgnoreCase("y")) {
             v.init(population);
         } else {
+            System.out.println("How many generations? ");
+            generations = s.nextInt();
             //Run the GA for how many generations specified 
             for (int i = 0; i < generations; i++) {
                 population = ga.Combination(population, population.size());
