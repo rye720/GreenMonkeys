428499f5d76ccba061904c3ba52a6cd80a648e8b
James Warth <jaw093@yahoo.com>
2016-01-27 18:32:53 -0500

    visual minor change

--- SimulationCPP/NaturalSelection/NaturalSelecton/Driver.cpp
+++ SimulationCPP/NaturalSelection/NaturalSelecton/Driver.cpp
@@ -7,6 +7,7 @@ int main() {
 	std::vector<Animal> population;
 	population = main.generatePop("bear", 5, 5, 1);
 
+	/*Loop i < x; x is the number of times you itterate through it*/
 	for (int i = 0; i < 35; i++){
 		main.combination(population, population.size(), i + 2);
 		main.ranking(population);
@@ -19,4 +20,23 @@ int main() {
 	
 	/*system("pause");*/
 	return 0;
+
+	/*
+	 WNDCLASSEX wndclass = { sizeof(WNDCLASSEX), CS_DBLCLKS, WindowProcedure,
+                            0, 0, GetModuleHandle(0), LoadIcon(0,IDI_APPLICATION),
+                            LoadCursor(0,IDC_ARROW), HBRUSH(COLOR_WINDOW+1),
+                            0, "Green Monkeys", LoadIcon(0,IDI_APPLICATION) } ;
+    if( RegisterClassEx(&wndclass) )
+    {
+        HWND window = CreateWindowEx( 0, myclass, "title",
+                   WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+                   CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, GetModuleHandle(0), 0 ) ;
+        if(window)
+        {
+            ShowWindow( window, SW_SHOWDEFAULT ) ;
+            MSG msg ;
+            while( GetMessage( &msg, 0, 0, 0 ) ) DispatchMessage(&msg) ;
+        }
+    }
+	*/
 }
