1aa85b3639e8c6d33551a733cda74601514026aa
Ryan Compton <rycompto@ius.edu>
2016-02-17 19:59:47 -0500

    rac - added command line arguments

--- SimulationCPP/NaturalSelection/NaturalSelecton/Driver.cpp
+++ SimulationCPP/NaturalSelection/NaturalSelecton/Driver.cpp
@@ -2,7 +2,8 @@
 #undef max
 
 
-int main() {
+int main(int argc, char* argv[]) {
+
 	GA main = GA();
 	GAUtils g = GAUtils();
 	Driver d = Driver();
@@ -10,55 +11,71 @@ int main() {
 	std::string animalName, visualConf;
 	int geneNum, animalStart, loops;
 
-	std::cout << "Enter the name of the animal: ";
-	std::cin >> animalName;
-	std::cout << std::endl;
-	std::cout << "Genes per animal: ";
-	d.defensiveInt(geneNum);
-	std::cout << std::endl;
-	std::cout << "Starting population: ";
-	d.defensiveInt(animalStart);
-	std::cout << std::endl;
-	std::cout << "Visual mode: ";
-	std::cin >> visualConf;
-	std::locale loc;
-	std::transform(visualConf.begin(), visualConf.end(), visualConf.begin(), ::tolower);
-	population = main.generatePop(animalName, animalStart, geneNum, 1);
-
-	if (visualConf == "yes" || visualConf == "y"){
-
-		//visual mode
-		for (int i = 0; i < 20; i++) {
-			main.combination(population, population.size(), i + 2);
-			main.ranking(population);
-			main.selection(population);
-		}
-		Visual v = Visual(population);
-		v.visualSetup();
+	// command line arguments supplied:
+	// animalName, geneNum, animalStart
+	if (argc == 5) {
+		animalName = std::string(argv[1]);
+		sscanf(argv[2], "%d", (int *)&geneNum);
+		sscanf(argv[3], "%d", (int *)&animalStart);
+		visualConf = std::string(argv[4]);
 	}
+	//no command line arguments
 	else {
 
-		//console window mode
+		std::cout << "Enter the name of the animal: ";
+		std::cin >> animalName;
 		std::cout << std::endl;
-		std::cout << "Simulate how many generations?: ";
-		d.defensiveInt(loops);
+		std::cout << "Genes per animal: ";
+		d.defensiveInt(geneNum);
+		std::cout << std::endl;
+		std::cout << "Starting population: ";
+		d.defensiveInt(animalStart);
+		std::cout << std::endl;
+		std::cout << "Visual mode: ";
+		std::cin >> visualConf;
 
-		/*Loop i < loops; loops is the number of times you iterate through it*/
-		for (int i = 0; i < loops; i++) {
-			main.combination(population, population.size(), i + 2);
-			main.ranking(population);
-			main.selection(population);
-		}
-		std::cout << std::endl;
-
-		/*g.popOut(population);*/
-		std::cout << "Top 10 individuals by gene ranking: ";
-		std::cout << std::endl;
-		g.popOut(population, 10);
-		std::cout << std::endl;
-		std::cout << "Population size: " + std::to_string(population.size());
-		std::cout << std::endl;
 	}
+
+		std::locale loc;
+		std::transform(visualConf.begin(), visualConf.end(), visualConf.begin(), ::tolower);
+		population = main.generatePop(animalName, animalStart, geneNum, 1);
+
+		if (visualConf == "yes" || visualConf == "y") {
+
+			//visual mode
+			for (int i = 0; i < 20; i++) {
+				main.combination(population, population.size(), i + 2);
+				main.ranking(population);
+				main.selection(population);
+			}
+			Visual v = Visual(population);
+			v.visualSetup();
+		}
+		else {
+
+			//console window mode
+			std::cout << std::endl;
+			std::cout << "Simulate how many generations?: ";
+			d.defensiveInt(loops);
+
+			/*Loop i < loops; loops is the number of times you iterate through it*/
+			for (int i = 0; i < loops; i++) {
+				main.combination(population, population.size(), i + 2);
+				main.ranking(population);
+				main.selection(population);
+			}
+			std::cout << std::endl;
+
+			/*g.popOut(population);*/
+			std::cout << "Top 10 individuals by gene ranking: ";
+			std::cout << std::endl;
+			g.popOut(population, 10);
+			std::cout << std::endl;
+			std::cout << "Population size: " + std::to_string(population.size());
+			std::cout << std::endl;
+		}
+
+	
 	/*system("pause");*/
 	return 0;
 }
